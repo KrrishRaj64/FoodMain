c4b7788ecb33ad0c01406c108ed1c58bfc220d6f
Vivek Neeralagi <139262048+VivekNeer@users.noreply.github.com>
2024-11-12 10:16:47 +0530

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { GiHamburgerMenu } from "react-icons/gi";
 import { themeChange } from 'theme-change';
-import { useEffect } from 'react';
-
 
 const Navbar = () => {
   useEffect(() => {
@@ -15,70 +13,65 @@ const Navbar = () => {
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">Food Paradise</a>
       </div>
-      {/* Navigation Links */}
-      <div className="hidden md:flex ">
+
+      {/* Mobile Navigation - Hamburger Dropdown */}
+      <div className="md:hidden flex-none z-10">
+        <div className="drawer drawer-end">
+          <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content">
+            {/* Open drawer button */}
+            <label htmlFor="my-drawer-4" className="drawer-button btn btn-ghost">
+              <GiHamburgerMenu />
+            </label>
+          </div>
+          <div className="drawer-side">
+            <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
+            <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+              {/* Sidebar content with navigation links */}
+              <li><Link to="/" className="btn btn-ghost">Home</Link></li>
+              <li><Link to="/menu" className="btn btn-ghost">Menu</Link></li>
+              <li><Link to="/contact" className="btn btn-ghost">Cart</Link></li>
+              <li><Link to="/about" className="btn btn-ghost">About</Link></li>
+
+              {/* Theme select */}
+              <li>
+                <Link to="/login" className="btn btn-ghost w-full">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup" className="btn btn-ghost w-full">Sign Up</Link>
+              </li>
+              <li>
+                <select data-choose-theme className="btn w-full">
+                  <option value="lemonade">Lemonade</option>
+                  <option value="coffee">Coffee</option>
+                  <option value="valentine">Valentine</option>
+                </select>
+              </li>
+
+
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      {/* Navigation Links (visible on desktop) */}
+      <div className="hidden md:flex">
         <Link to="/" className="btn btn-ghost">Home</Link>
         <Link to="/menu" className="btn btn-ghost">Menu</Link>
         <Link to="/contact" className="btn btn-ghost">Cart</Link>
         <Link to="/about" className="btn btn-ghost">About</Link>
-      </div>
-      <div>
-      <select data-choose-theme className="btn hidden lg:block">
-          <option value="lemonade">Luxury</option>
-          <option value="coffee">Cupcake</option>
-          <option value="valentine">Cyberpunk</option>
-        </select>
-      </div>
 
-      {/* Mobile Navigation - Hamburger Dropdown */}
-      <div className="md:hidden flex-none z-10">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <details>
-              <summary><GiHamburgerMenu /></summary>
-              <ul className="bg-base-100 rounded-t-none p-2">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/menu">Menu</Link></li>
-                <li><Link to="/contact">Cart</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li>
-              <select data-choose-theme className="btn w-full">
-                <option value="lemonade">Luxury</option>
-                <option value="coffee">Cupcake</option>
-                <option value="valentine">Cyberpunk</option>
-              </select>
-            </li>
-              </ul>
-            </details>
-          </li>
-        </ul>
+        {/* Theme select */}
+        <select data-choose-theme className="btn hidden lg:block">
+          <option value="lemonade">Lemonade</option>
+          <option value="coffee">Coffee</option>
+          <option value="valentine">Valentine</option>
+        </select>
+
+        {/* Login / Sign Up buttons on desktop */}
+        <Link to="/login" className="btn btn-ghost ml-2">Login</Link>
+        <Link to="/signup" className="btn btn-ghost ml-2">Sign Up</Link>
       </div>
-      <div className="flex-none gap-2">
-      {/* Login / Sign Up Dropdown */}
-      <div className="dropdown dropdown-end">
-        <div tabIndex={0} role="button" className="btn m-1">
-          Login / Sign Up
-        </div>
-        <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-          <li>
-            <Link
-              to="/login"
-              className="hover:bg-gray-200 p-2 rounded"
-            >
-              Login
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/signup"
-              className="hover:bg-gray-200 p-2 rounded"
-            >
-              Sign Up
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
     </div>
   );
 };
